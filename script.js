dbaa8ff2760378c6cca52a0e8994d59ac4f1337b
martavarika18 <marta.varika@inbox.lv>
2022-01-24 09:15:39 +0200

    gala

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ function nolasa() {
     const m3 = parseFloat(mala3.value);
 
     if (m1 > 0 && m2 > 0 && m3 > 0) {//pārbauda vai malu gaurmi ir lielāki par 0
-        console.log({ m1, m2, m3 });
+        console.log({ m1, m2, m3 });//konsolē izvada malu garumus
         return { m1, m2, m3 };
     } else {
-        console.log(false);
+        console.log(false);//konsolē izdrukā false, ja kāda no malām ir 0
         return false;
     }
 }
@@ -17,45 +17,30 @@ function nolasa() {
 function perimetrs(m1, m2, m3) {
 
     const m = nolasa();
-
     m1 = m.m1; m2 = m.m2; m3 = m.m3;
-
     console.log(m1, m2, m3);
-
-    const p = m1 + m2 + m3;
-
+    const p = m1 + m2 + m3;//tiek aprēķināts perimetrs
     console.log(p);
-
     return (p);
 }
 
 function pusper(m1, m2, m3) {
-
-    const pusper = perimetrs(m1, m2, m3) / 2;
-
+    const pusper = perimetrs(m1, m2, m3) / 2;//tiek aprēķināts pusperimetrs
     console.log(pusper);
-
     return (pusper);
 
 }
 function laukums(m1, m2, m3) {
-
     const pusper = perimetrs(m1, m2, m3) / 2;
-
-    let tlaukums = Math.sqrt(
-
-        pusper * ((pusper - m1) * (pusper - m2) * (pusper - m3)));
-
+    let tlaukums = Math.sqrt(pusper * ((pusper - m1) * (pusper - m2) * (pusper - m3)));//aprēķina laukumu
     console.log(tlaukums);
-
     return tlaukums;
 
 }
 function irTrijsturis(m1, m2, m3) {
 
 
-    if (m1 < m2 + m3 && m2 < m1 + m3 && m3 < m1 + m2) {
-
+    if (m1 < m2 + m3 && m2 < m1 + m3 && m3 < m1 + m2) {//pārbauda vai jebkuru 2 malu garumu summa ir lielāka par trešo malu
         return true;
     } else {
 
@@ -68,17 +53,17 @@ function rezultats() {
     m1 = m.m1; m2 = m.m2; m3 = m.m3;
     console.log(m1, m2, m3);
     if (!nolasa()) {
-        t = "Ievadītie dati ir nekorekti, kāda no malām ir 0 ";
+        t = "Ievadītie dati ir nekorekti, kāda no malām ir 0. ";
     } else {
         if (irTrijsturis(m1, m2, m3) == false) {
-            t = "Trijstūris neeksistē, jo jebkuru 2 malu garumu summai ir jābūt lielākai par trešo malu!";
+            t = "Trijstūris neeksistē, jo jebkuru 2 malu garumu summai ir jābūt lielākai par trešo malu.";
         } else {
-            t = "Trijstūris ar malu garumiem " + m1 + " , " + m2 + " , " + m3 + " eksistē. ";
-            if (m1 == m2 && m2 == m3) {
-                t += "Vienādmalu trijstūris. ";
+            t = "Trijstūris ar malu garumiem " + m1 + " , " + m2 + " , " + m3 + " eksistē. ";//izdrukā info par trijstūriem katram ievadītajam trijstūrim ar ievadītajiem malu garumiem.
+            if (m1 == m2 && m2 == m3) { //pārbauda vai divas malas ir vienādas
+                t += "Tas ir vienādmalu trijstūris. ";
             } else {
-                if (m1 == m2 || m2 == m3 || m1 == m3) {
-                    t += "Vienādsānu trijstūris. ";
+                if (m1 == m2 || m2 == m3 || m1 == m3) {//pārbauda vai visas trīs malas ir vienādas
+                    t += "Tas ir vienādsānu trijstūris. ";
                 }
             }
 
